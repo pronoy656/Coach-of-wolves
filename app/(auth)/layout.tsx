a3a0f42bde3670e65c79dcc855de7ae50d4d93a1
Pronoy <pronoypaul56@gmail.com>
2026-05-24 00:52:14 +0600

    dark mode issue fixed on dashboard

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,5 +3,5 @@ export default function AuthLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <div>{children}</div>;
+  return <div className="bg-background min-h-screen">{children}</div>;
 }
